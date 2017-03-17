6ed43c186e2e1dfc2c63ed8c17544375cda0ed9d
brentlobbezoo <brentlobbezoo@hotmail.com>
2017-03-17 14:27:40 +0100

    add form to post messages update readme update routing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import PostForm from './components/PostForm';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom'
+import { Route } from 'react-router'
 
 ReactDOM.render(
-  <App />,
+    <BrowserRouter>
+        <div>
+            <Route exact path="/" component={App} />
+            <Route path="/add" component={PostForm} />
+        </div>
+    </BrowserRouter>,
   document.getElementById('root')
 );
